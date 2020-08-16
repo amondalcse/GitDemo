75ee7e927ac767da1a369928fa592a1ce2ae13ca
Abhijit <amondalcse@gmail.com>
2020-08-16 14:19:24 +0530

    second commit gitstuffs

---
+++ src/test/java/com/test/tests/VerifygitCode.java
@@ -0,0 +1,10 @@
+package com.test.tests;
+
+public class VerifygitCode 
+{
+	public static void main(String[] args) {
+		System.out.println("first chages");
+		System.out.println("Second changes");
+	}
+
+}
